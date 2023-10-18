e6f76bf39dbf725cbd651db3b5bec01a0547bc76
Ramazan  TOY <ramcotoy06@gmail.com>
2023-10-18 17:39:40 +0300

    camera follow, cube handler

---
+++ Assets/_Project/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetTransform;
+    [SerializeField]
+    private float _followSpeed = 5.0f;
+
+    private float zOffset;
+
+    private void Start()
+    {
+        zOffset = transform.position.z - _targetTransform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        float targetZPosition = _targetTransform.position.z + zOffset;
+
+        Vector3 newPosition = transform.position;
+        newPosition.z = Mathf.Lerp(transform.position.z, targetZPosition, _followSpeed * Time.fixedDeltaTime);
+        transform.position = newPosition;
+    }
+}
